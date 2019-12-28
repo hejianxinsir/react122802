8c0dd5944fc17b81028aebb0874f0323f46f2a76
hejianxin <hejianxinsir@gmail.com>
2019-12-28 17:24:24 +0800

    添加样式，有基本的样子了

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import logo from './logo.svg';
 import './App.css';
 import TodoInput from './TodoInput';
 import TodoItem from './TodoItem';
+import 'normalize.css';
+import './reset.css';
 
 class App extends React.Component {
 	constructor(props){
