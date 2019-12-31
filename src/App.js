1c8f6b99dfe73a18ef638352d09a1aa1960ae723
hejianxin <hejianxinsir@gmail.com>
2019-12-31 10:30:11 +0800

    完成全部功能

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import TodoInput from './TodoInput';
 import TodoItem from './TodoItem';
 import 'normalize.css';
 import './reset.css';
-
+import UserDialog from './UserDialog';
+import {getCurrentUser, signOut} from './leanCloud';
 
 class App extends React.Component {
 	constructor(){
 		super()
 		this.state = {
+			user: getCurrentUser() || {},
 			newTodo: '',
 			todoList: []
 		}
@@ -29,7 +31,9 @@ class App extends React.Component {
 		})
   	return (
   	  <div className="App">
-				<h1>我的待办</h1>
+				<h1>{this.state.user.username || '我'}的待办
+					{this.state.user.id ? <button onClick={this.signOut.bind(this)}>退出</button> : null}
+				</h1>
 				<div className="inputWrapper">
 					<TodoInput content={this.state.newTodo}
 							onChange={this.changeTitle.bind(this)}
@@ -39,8 +43,23 @@ class App extends React.Component {
 				<ol className="todoList">
 					{todos}
 				</ol>
+				{this.state.user.id ? null : <UserDialog onSignUp={this.onSignUpOrSignIn.bind(this)}
+																								 onSignIn={this.onSignUpOrSignIn.bind(this)}/>}
   	  </div>
 		);
+	}
+
+	signOut(){
+		signOut()
+		let stateCopy = JSON.parse(JSON.stringify(this.state))
+		stateCopy.user = {}
+		this.setState(stateCopy)
+	}
+
+	onSignUpOrSignIn(user){
+		let stateCopy = JSON.parse(JSON.stringify(this.state))
+		stateCopy.user = user
+		this.setState(stateCopy)
 	}
 
 	componentDidUpdate(){
--- src/leanCloud.js
+++ src/leanCloud.js
@@ -1,4 +1,3 @@
-
 var AV = require('leancloud-storage');
 
 AV.init({
@@ -8,4 +7,48 @@ serverURLs: "https://leancloud.cn"
 });
 
 export default AV;
+
+export function signUp(username,password,successFn,errorFn){
+	var user = new AV.User()
+	user.setUsername(username)
+	user.setPassword(password)
+	user.signUp().then(function(loginedUser){
+		let user = getUserFromAVUser(loginedUser)
+		successFn.call(null, user)
+	}, function(error){
+		errorFn.call(null, error)
+	})
+
+	return undefined
+}
+
+export function getCurrentUser(){
+	let user = AV.User.current()
+	if(user){
+		return getUserFromAVUser(user)
+	}else{
+		return null
+	}
+}
+
+export function signOut(){
+	AV.User.logOut()
+	return undefined
+}
+
+export function signIn(username, password, successFn, errorFn){
+	AV.User.logIn(username, password).then(function(loginedUser){
+		let user = getUserFromAVUser(loginedUser)
+		successFn.call(null, user)
+	},function(error){
+		errorFn.call(null, error)
+	})
+}
+
+function getUserFromAVUser(AVUser){
+	return {
+		id: AVUser.id,
+		...AVUser.attributes
+	}
+}
 
